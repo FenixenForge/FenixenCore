9000ad20327e090381756cefbd5f4422fd847dc2
FenixenForge <fenixneforge@gmail.com>
2025-03-31 23:15:22 -0600

    Utils Messages.java

--- src/main/java/com/fenixenforge/FenixenCore/Utils/Messages.java
+++ src/main/java/com/fenixenforge/FenixenCore/Utils/Messages.java
@@ -3,26 +3,25 @@ package com.fenixenforge.FenixenCore.Utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
 public class Messages {
     private static CommandSender sender;
-
-    public static final Pattern HEX = Pattern.compile("#([A-Fa-f0-9]{6})");
+    private static final Pattern HEX = Pattern.compile("#([A-Fa-f0-9]{6})");
 
     public static String Color(String message) {
         Matcher matcher = HEX.matcher(message);
         StringBuffer buffer = new StringBuffer();
 
         while (matcher.find()) {
-            matcher.appendReplacement(buffer, ChatColor.of("#" + matcher.group(1)).toString());
+            String hexColor = matcher.group(1);
+            matcher.appendReplacement(buffer, ChatColor.of("#" + hexColor).toString());
         }
 
         matcher.appendTail(buffer);
-        message = buffer.toString();
-
-        return org.bukkit.ChatColor.translateAlternateColorCodes('&', message);
+        return org.bukkit.ChatColor.translateAlternateColorCodes('&', buffer.toString());
     }
 
     public static void Console(String message) {
@@ -36,4 +35,9 @@ public class Messages {
     public static void Sender(String message) {
         sender.sendMessage(Color(message));
     }
+
+    public static TextComponent getTextComponent(String message) {
+        return new TextComponent(Color(message));
+    }
 }
+
